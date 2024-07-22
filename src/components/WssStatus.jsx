91306e0c6b1f1be57ae270c7dc3523ab489d2aec
B <B@Aravinths-MacBook-Pro.local>
2024-07-22 21:57:11 +0100

    fix

--- src/components/WssStatus.jsx
+++ src/components/WssStatus.jsx
@@ -3,75 +3,12 @@ import "./WssStatus.css";
 import { ApiPromise, WsProvider } from "@polkadot/api";
 
 const WssStatus = () => {
-  const [endpointsStatus, setEndpointsStatus] = useState([]);
 
-  const endpoints = [
-    "wss://turing-testnet.avail-rpc.com",
-    "wss://avail-turing.public.blastapi.io",
-    "wss://avail-turing-rpc.publicnode.com",
-    "wss://turing.public.curie.radiumblock.co",
-    "wss://avail-turing-rpc.openbitlab.com",
-    "wss://avail-turing.bountyblok.io",
-    "wss://rpc-t.avail.nodestake.org"
-  ];
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const results = [];
-      for (const endpoint of endpoints) {
-        let success = false;
-        let errorMessages = [];
-        for (let attempt = 0; attempt < 3; attempt++) {
-          try {
-            const wsProvider = new WsProvider(endpoint);
-            const api = await ApiPromise.create({ provider: wsProvider });
-
-            // If we succeed, disconnect and exit the retry loop
-            await api.disconnect();
-            success = true;
-            results.push({ endpoint, status: "Active" });
-            break;
-          } catch (error) {
-            errorMessages.push(`Attempt ${attempt + 1}: ${error.message}`);
-            if (attempt === 2) { // Last attempt
-              results.push({ endpoint, status: "Inactive", error: errorMessages.join("; ") });
-            }
-          }
-        }
-      }
-      setEndpointsStatus(results);
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <div className="wssStatus">
-      <h1>WebSocket Endpoint Status</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Endpoint</th>
-            <th>Status</th>
-            <th>Error Messages</th>
-          </tr>
-        </thead>
-        <tbody>
-          {endpointsStatus.length > 0 ? (
-            endpointsStatus.map((endpoint, index) => (
-              <tr key={index}>
-                <td>{endpoint.endpoint}</td>
-                <td>{endpoint.status}</td>
-                <td>{endpoint.error || "None"}</td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="3">Checking endpoints...</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      <h1>WebSocket Endpoint Status ** WIP ** </h1>
+     
     </div>
   );
 };
